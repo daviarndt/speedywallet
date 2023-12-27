4ac902b10dd671337a66643f7824a54a25cc17b8
Davi Arndt <daviarndt@MacBook-Air-de-Davi.local>
2023-12-27 01:52:25 -0300

    Initial Commit

---
+++ src/main/java/com/speedywallet/speedywallet/SpeedywalletApplication.java
@@ -0,0 +1,13 @@
+package com.speedywallet.speedywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedywalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedywalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speedywallet/speedywallet/SpeedywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedywallet.speedywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
